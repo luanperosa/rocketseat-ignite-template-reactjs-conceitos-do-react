82f3d46fd765fdf717a2c5d29c63bc1131335c76
Luan Perosa Chitto <luan.perosa@hotmail.com>
2021-08-26 20:59:34 -0300

    Chalange Complete

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -6,7 +6,7 @@ import { FiTrash, FiCheckSquare } from 'react-icons/fi'
 
 interface Task {
   id: number;
-  title: string;
+  title?: string;
   isComplete: boolean;
 }
 
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+    
+    const task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks(oldState => [...oldState, task]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksIsComplete = tasks.map(task => {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete;
+      } 
+      return task;
+    });
+
+    setTasks(tasksIsComplete);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const taskChecked = tasks.filter(task => task.id !== id);
+    setTasks(taskChecked);
   }
 
   return (
